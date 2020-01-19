16abfeab459de05266f145d59c3207a9fb1b5dd6
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-19 08:39:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   array={}
    dwarves.each_with_index{|item, index| array[item]=index}
-  array
+   puts "#{}"
 end
 
 def summon_captain_planet(summon_captain_planet)# code an #argument here
