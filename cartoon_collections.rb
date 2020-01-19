4840f7e9afdc2a3ad1699771d0486ff3498d5e0a
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-19 08:05:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarves)# code an argument here
   array
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(summon_captain_planet)# code an argument here
+
   # Your code here
   
 end
