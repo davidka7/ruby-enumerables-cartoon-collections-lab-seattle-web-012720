6ab65f3751c031f385c4d4d4a6f73f13f74c9316
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-19 07:58:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  array={}
+   dwarves.each_with_index{|item,index| array[item]=index}
+  array
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
