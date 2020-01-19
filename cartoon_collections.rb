e949db3f278bd94cf05633c5dd8da9da78ce874a
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-19 08:29:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(short_words)# code an argument #here
 
 end
 
-def find_the_cheese(snakcs)# code an argument here
+def find_the_cheese(snack)# code an argument here
   # the array below is here to help
   
   cheese_types = ["cheddar", "gouda", "camembert"]
